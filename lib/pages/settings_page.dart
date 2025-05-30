29fba501eb3db562c5205165a12fa787f45eef3b
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-05-30 21:14:32 +0100

    settings page added

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_deliver/components/settings_tile.dart';
+import 'package:food_deliver/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -7,15 +11,59 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Settings",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          )
+        title: Text(
+          "Settings",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        children: [
+
+          //Dark mode
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: SettingsTile(
+                  text: "Dark Mode", 
+                  description: "Switch between light and dark mode", 
+                  icon: Icons.dark_mode
+                ),
+              ),
+              CupertinoSwitch(
+                value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode, 
+                onChanged: (value){
+                  Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+                },
+                ),
+              SizedBox(width: 10,),
+            ],
+          ),
+
+          const Spacer(),
+          
+          Row(
+            children: [
+              Expanded(
+                child: SettingsTile(
+                  text: "Delete account", 
+                  description: "All your datas would be lost", 
+                  icon: Icons.delete,
+                  onTap: () {
+                    
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+
+        ],
+      ),
+
     );
   }
 }
